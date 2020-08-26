fd0523f0e186ae669eb3b60c051dc871ac20637c
George Antoniadis <g.antoniadis@paymentcomponents.com>
2020-08-26 15:48:51 +0300

    48

---
+++ src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
@@ -0,0 +1,31 @@
+package guru.springframework.sfgpetclinic.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PersonTest {
+
+    @Test
+    void groupAssertions() {
+        //given
+        Person person = new Person(1l, "Joe", "Buck");
+        //when
+        assertAll("Test Props Set",
+                () -> assertEquals("Joe", person.getFirstName()),
+                () -> assertEquals("Buck", person.getLastName())
+        );
+    }
+
+    @Test
+    void groupAssertionsMsgs() {
+        //given
+        Person person = new Person(1l, "Joe", "Buck");
+        //when
+        assertAll("Test Props Set",
+                () -> assertEquals("Joe", person.getFirstName(), "First Name Failed"),
+                () -> assertEquals("Buck", person.getLastName(), "Last Name Failed")
+        );
+    }
+}
